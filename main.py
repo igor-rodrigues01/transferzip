df132b12bc5f6ae37466eba48cf06677d8e1c96f
igor-rodrigues01 <igor.silva@hexgis.com>
2017-07-12 10:15:10 -0500

    iserted datetime

--- main.py
+++ main.py
@@ -4,26 +4,36 @@ import zipfile
 import os
 
 class Main():
-	_now = None
+	_now  = None
+	_path_zip = None
 
-	def __init__(self,path='teste.txt'):
-		self.delete_zip()
-		self.create_zip(path)
-		self._now = datetime.now()
+	def __init__(self,path_zip='teste.txt'):
+		self._path_zip = path_zip
+		self._now      = datetime.now()
 
-	def create_zip(self,path):
-		zf = zipfile.ZipFile(path,'w')
-		zf.write(path,compress_type=zipfile.ZIP_DEFLATED)
+	def create_zip(self,path_zip):
+		zf = zipfile.ZipFile("shape_file.zip",'w')
+		zf.write(path_zip,compress_type=zipfile.ZIP_DEFLATED)
 		zf.close()
 
-	def delete_zip(self,path="."):
-	    files = os.listdir(path)
+	def delete_zip(self,path_dir="."):
+	    files = os.listdir(path_dir)
+	    
 	    for f in files:
 	    	if f != 'main.py':
-	    		os.remove(f)  
+	    		os.remove(f) 
+ 
+	def main(self):
+		hrs = self._now.hour
+		path = self._path_zip
+
+		if hrs == 00 or hrs == 0:
+			# self.delete_zip()
+			self.create_zip(path)
+
 
 
 
 if __name__ == '__main__':
    m = Main()
-   m.delete()
+   m.main()
